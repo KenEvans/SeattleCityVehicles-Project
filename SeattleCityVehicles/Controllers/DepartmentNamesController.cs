238e9cf95479ed2975ee3ff7e46553ea02cac25c
KenEvans <ken.evans@bellevuecollege.edu>
2021-06-05 13:13:07 -0700

    Working on query 3, got dropdown populated from db using DepartmentNamesController

---
+++ SeattleCityVehicles/Controllers/DepartmentNamesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SeattleCityVehicles.Controllers
+{
+    public class DepartmentNamesController : ApiController
+    {
+        /// <summary>
+        /// Get method to return all the department names for the dropdown (select) menu
+        /// Returns the department names
+        /// Written by Ken Evans
+        /// </summary>
+
+        SeattleCityVehiclesEntities myDB = new SeattleCityVehiclesEntities();
+
+        [HttpGet]
+        public IEnumerable<string> GetDepartmentNames()
+        {
+            var allDepartments = (from deptNames in myDB.DepartmentNames
+                                  select deptNames.DeptName).Distinct().AsEnumerable();
+            return allDepartments;
+        }
+    }
+}
--- SeattleCityVehicles/Scripts/App1.js
+++ SeattleCityVehicles/Scripts/App1.js
@@ -1,21 +1,21 @@
 ﻿
 $(document).ready(function () {
-    GetSalesPeople();
-    GetStores();
+    GetDepartmentNames();
+    //GetStores();
 });
 
 
 // Written by Ken Evans
-function GetMarkups() {
+function GetDeptValues() {
     // Send an AJAX request
-    $.get('api/Markups')
-        .done(function (data) {
-            $('#markup').empty();
-            console.log(data);
-            $.each(data, function (key, item) {
-                $('<li>', { text: formatItem(item) }).appendTo($('#markup'));
-            });
-        });
+    //$.get('api/Markups')
+    //    .done(function (data) {
+    //        $('#markup').empty();
+    //        console.log(data);
+    //        $.each(data, function (key, item) {
+    //            $('<li>', { text: formatItem(item) }).appendTo($('#markup'));
+    //        });
+    //    });
 }
 
 
@@ -25,49 +25,50 @@ function formatItem(item) {
 }
 
 
-function GetSalesPeople() {
+//Written by Ken Evans
+function GetDepartmentNames() {
     // Send an AJAX request
-    $.getJSON("api/SalesPeople")
+    $.getJSON("api/DepartmentNames")
         .done(function (data) {
             $.each(data, function (key, item) {
-                $('<option>', { text: item, value: item }).appendTo($('#chooseEmployee'));
+                $('<option>', { text: item, value: item }).appendTo($('#chooseDepartment'));
             });
         });
 }
 
 
-function GetStores() {
-    // Send an AJAX request
-    $.getJSON("api/Stores")
-        .done(function (data) {
-            $.each(data, function (key, item) {
-                $('<option>', { text: item, value: item }).appendTo($('#chooseStore'));
-            });
-        });
-}
+//function GetStores() {
+    //// Send an AJAX request
+    //$.getJSON("api/Stores")
+    //    .done(function (data) {
+    //        $.each(data, function (key, item) {
+    //            $('<option>', { text: item, value: item }).appendTo($('#chooseStore'));
+    //        });
+    //    });
+//}
 
 
-function GetAnnualSales() {
-    let onFocus = document.getElementById("chooseEmployee");
-    let employeeName = onFocus.options[onFocus.selectedIndex].value;
+//function GetAnnualSales() {
+    //let onFocus = document.getElementById("chooseEmployee");
+    //let employeeName = onFocus.options[onFocus.selectedIndex].value;
 
-    // Send an AJAX request
-    $.getJSON('api/AnnualSales?salesPeople=' + employeeName)
-        .done(function (data) {
-            console.log(data);
-            document.getElementById("getEmpSales").innerText = "This employee sold $" + data + " for the year";
-        });
-}
+    //// Send an AJAX request
+    //$.getJSON('api/AnnualSales?salesPeople=' + employeeName)
+    //    .done(function (data) {
+    //        console.log(data);
+    //        document.getElementById("getEmpSales").innerText = "This employee sold $" + data + " for the year";
+    //    });
+//}
 
 
-function GetStoreSales() {
-    let onFocus = document.getElementById("chooseStore");
-    let storeName = onFocus.options[onFocus.selectedIndex].value;
+//function GetStoreSales() {
+    //let onFocus = document.getElementById("chooseStore");
+    //let storeName = onFocus.options[onFocus.selectedIndex].value;
 
-    // Send an AJAX request
-    $.getJSON("api/StoreSales?storeCity=" + storeName)
-        .done(function (data) {
-            console.log(data);
-            document.getElementById("storeSales").innerText = "That store sold $" + data + " for the year";
-        });
-}
+    //// Send an AJAX request
+    //$.getJSON("api/StoreSales?storeCity=" + storeName)
+    //    .done(function (data) {
+    //        console.log(data);
+    //        document.getElementById("storeSales").innerText = "That store sold $" + data + " for the year";
+    //    });
+//}
